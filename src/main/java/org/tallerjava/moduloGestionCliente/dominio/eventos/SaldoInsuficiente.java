5f93d9314f121284c2b0621a97e34a226f90b397
Samuel Mindler <mindlersamuel@gmail.com>
2024-05-14 19:13:53 -0300

    union modulos sucive gestion peaje pagos

--- src/main/java/org/tallerjava/moduloGestionCliente/dominio/eventos/SaldoInsuficiente.java
+++ src/main/java/org/tallerjava/moduloGestionCliente/dominio/eventos/SaldoInsuficiente.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.tallerjava.moduloGestionCliente.dominio.clases.CuentaPREPaga;
-import org.tallerjava.moduloGestionCliente.dominio.clases.Usuario;
-import org.tallerjava.moduloGestionCliente.dominio.clases.Vehiculo;
 
 @Getter
 @Setter
